800ea670d043f6cb7ea3889a286b4f577fb91ac4
Hemalatha14 <39404986+Hemalatha14@users.noreply.github.com>
2018-05-19 01:52:23 -0700

    Delete pangram.py

--- pangram.py
+++
@@ -1,9 +0,0 @@
-string=input("enter sentence")
-stringd=set(string)
-list = []
-for i in range(97,123):
-    list.append(chr(i))
-if stringd in list:
-    print("pangram")
-else:
-    print("not pangram")
